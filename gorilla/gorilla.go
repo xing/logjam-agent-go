131ef5dddba5124bd61acb1b7bb56f94a805356b
Stefan Kaes <stefan.kaes@xing.com>
2020-05-03 13:47:26 +0200

    support declaring logjam action names manually

--- gorilla/gorilla.go
+++ gorilla/gorilla.go
@@ -10,128 +10,69 @@ import (
 	"github.com/xing/logjam-agent-go"
 )
 
+// handler encapsulates the original handler and information how to determine the full
+// action name for a request.
+type handler struct {
+	action       string       // the precomputed action name
+	appendMethod bool         // whether the action needs to be augmented by the HTTP request method in lowercase
+	handler      http.Handler // the original handler
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logjamRequest := logjam.GetRequest(r.Context())
+	if logjamRequest != nil {
+		logjamRequest.Action = h.actionName(r.Method)
+	}
+	h.handler.ServeHTTP(w, r)
+}
+
+func (h *handler) actionName(method string) string {
+	if h.appendMethod {
+		return h.action + "#" + strings.ToLower(method)
+	}
+	return h.action
+}
+
+// ActionName registers a logjam action name for the given mux route. Requires a logjam
+// compatible action name of the form (Module::)*Controller#action.
+func ActionName(route *mux.Route, actionName string) {
+	route.Handler(handler{
+		action:       actionName,
+		appendMethod: !strings.Contains(actionName, "#"),
+		handler:      route.GetHandler(),
+	})
+}
+
 // Options provides parameters to the gorilla mux.Route to logjam action name conversion.
 type Options struct {
-	UseRouteNames   bool // If true, logjam routes are based on route names set with the r.Name() call.
 	ForceUniqueness bool // If true, setting up name extraction will panic when routes are not unique.
 	CheckOnly       bool // If true, just perfroms a dry run and prints the resulting routes.
 }
 
-// Options are accessible to all functions in the package.
-var opts *Options
-
-// Routes are collected during setup and checked for uniqueness if required.
-var routes = []routeInfo{}
-
-type routeInfo struct {
-	action       string     // the computed action name prefix
-	route        *mux.Route // the corresponding route
-	appendMethod bool       // whether to append the HTTP method to the route
-}
-
-func routeSeen(route *mux.Route) bool {
-	for _, r := range routes {
-		if r.route == route {
-			return true
-		}
-	}
-	return false
-}
-
-func (ri *routeInfo) actionName() string {
-	if ri.appendMethod {
-		return ri.action + "#:method"
-	}
-	return ri.action
-}
-
-func sortRoutes() {
-	sort.Slice(routes, func(i, j int) bool {
-		a, _ := routes[i].route.GetPathTemplate()
-		b, _ := routes[j].route.GetPathTemplate()
-		return a < b
-	})
-}
-
-func maxRouteLength() int {
-	l := 0
-	for _, r := range routes {
-		template, _ := r.route.GetPathTemplate()
-		if len(template) > l {
-			l = len(template)
-		}
-	}
-	return l
-}
-
-func padRight(s string, l int) string {
-	n := len(s)
-	if n >= l {
-		return s
-	}
-	return s + strings.Repeat(" ", l-n)
-}
-
-func printRoutes() {
-	n := maxRouteLength()
-	fmt.Printf("\n============== logjam routes ===============\n")
-	for _, r := range routes {
-		methods, _ := r.route.GetMethods()
-		ms := strings.Join(methods, " ")
-		if ms == "" {
-			ms = "ALL"
-		}
-		template, _ := r.route.GetPathTemplate()
-		fmt.Printf("%-10s  %s  %s\n", ms, padRight(template, n), r.actionName())
-	}
-}
-
-// SetupActionNameExtraction traverses all routes of the given router and replaces the
-// corresponding handler with a new handler that uses the route name or path template to
-// compute better logjam action names. It must be called after all routes have been set up
+// SetupRoutes traverses all routes of the given router and replaces handlers which have
+// no logjam action name attached yet handler with a new handler that uses an action name
+// derived from the path template to. It must be called after all routes have been set up
 // on the router.
-func SetupActionNameExtraction(r *mux.Router, options *Options) {
-	opts = options
+func SetupRoutes(r *mux.Router, options *Options) {
 	r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		if routeSeen(route) {
+		h := route.GetHandler()
+		if h == nil {
+			return nil
+		}
+		if _, isLogjamHandler := h.(handler); isLogjamHandler {
 			return nil
 		}
 		action, appendMethod := actionName(route)
 		if action == "" {
 			return nil
 		}
-		if options.CheckOnly {
-			routes = append(routes, routeInfo{action: action, route: route, appendMethod: appendMethod})
-		}
 		route.Handler(handler{
 			action:       action,
 			appendMethod: appendMethod,
-			handler:      route.GetHandler(),
+			handler:      h,
 		})
 		return nil
 	})
-	if options.CheckOnly {
-		sortRoutes()
-		printRoutes()
-		routes = []routeInfo{}
-	}
-}
-
-type handler struct {
-	action       string
-	handler      http.Handler
-	appendMethod bool
-}
-
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	logjamRequest := logjam.GetRequest(r.Context())
-	if logjamRequest != nil {
-		logjamRequest.Action = h.action
-		if h.appendMethod {
-			logjamRequest.Action += "#" + strings.ToLower(r.Method)
-		}
-	}
-	h.handler.ServeHTTP(w, r)
 }
 
 func actionName(route *mux.Route) (string, bool) {
@@ -186,4 +127,71 @@ func actionNameParts(path string) ([]string, bool) {
 		parts = append(parts, formatSegment(part))
 	}
 	return parts, true
+}
+
+// routeInfo is just for printing routes
+type routeInfo struct {
+	route   *mux.Route // the corresponding route
+	handler handler    // the logjam handler
+}
+
+// PrintRoutes prints the routes and their logjam action names.
+func PrintRoutes(r *mux.Router) {
+	routes := []routeInfo{}
+	r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		h := route.GetHandler()
+		if h == nil {
+			return nil
+		}
+		if lh, isLogjamHandler := h.(handler); isLogjamHandler {
+			routes = append(routes, routeInfo{route: route, handler: lh})
+		}
+		return nil
+	})
+	sortRoutes(routes)
+	printRoutes(routes)
+}
+
+func sortRoutes(routes []routeInfo) {
+	sort.Slice(routes, func(i, j int) bool {
+		a, _ := routes[i].route.GetPathTemplate()
+		b, _ := routes[j].route.GetPathTemplate()
+		return a < b
+	})
+}
+
+func printRoutes(routes []routeInfo) {
+	n := maxRouteLength(routes)
+	fmt.Printf("\n============================ logjam routes ================================\n")
+	for _, r := range routes {
+		methods, _ := r.route.GetMethods()
+		if len(methods) == 0 {
+			template, _ := r.route.GetPathTemplate()
+			fmt.Printf("%-10s  %s  %s\n", "ALL", padRight(template, n), r.handler.actionName(":method"))
+			continue
+		}
+		for _, m := range methods {
+			template, _ := r.route.GetPathTemplate()
+			fmt.Printf("%-10s  %s  %s\n", m, padRight(template, n), r.handler.actionName(m))
+		}
+	}
+}
+
+func maxRouteLength(routes []routeInfo) int {
+	l := 0
+	for _, r := range routes {
+		template, _ := r.route.GetPathTemplate()
+		if len(template) > l {
+			l = len(template)
+		}
+	}
+	return l
+}
+
+func padRight(s string, l int) string {
+	n := len(s)
+	if n >= l {
+		return s
+	}
+	return s + strings.Repeat(" ", l-n)
 }
--- gorilla/gorilla_test.go
+++ gorilla/gorilla_test.go
@@ -2,7 +2,6 @@ package gorilla
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -19,18 +18,28 @@ import (
 func TestGorillaNameExtraction(t *testing.T) {
 	router := mux.NewRouter()
 
-	router.Path("/rest/users/{user_id}").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	somebody := func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte(`some body`))
-	})
-	router.Path("/rest/users").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte(`some body`))
-	}).Methods("GET", "POST")
-	router.Path("/rest/schnurzes/{schnurz_id}/users").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte(`some body`))
-	})
+	}
+
+	ActionName(router.Path("/rest/users").Methods("GET").HandlerFunc(somebody), "Rest::Users#index")
+	ActionName(router.Path("/rest/users").Methods("POST").HandlerFunc(somebody), "Rest::Users#create")
+	ActionName(router.Path("/rest/users/{user_id}").Methods("GET").HandlerFunc(somebody), "Rest::Users#show")
+	ActionName(router.Path("/rest/users/{user_id}").Methods("PUT", "PATCH").HandlerFunc(somebody), "Rest::Users#update")
+	ActionName(router.Path("/rest/users/{user_id}").Methods("DELETE").HandlerFunc(somebody), "Rest::Users#destroy")
+	ActionName(router.Path("/rest/users/{user_id}/comrades").Methods("GET").HandlerFunc(somebody), "Rest::Users#comrades")
+
+	sub := router.PathPrefix("/web").Subrouter()
+	ActionName(sub.Path("/users").Methods("GET").HandlerFunc(somebody), "Web::Users#index")
+	ActionName(sub.Path("/users").Methods("POST").HandlerFunc(somebody), "Web::Users#create")
+	ActionName(sub.Path("/users/{user_id}").Methods("GET").HandlerFunc(somebody), "Web::Users#show")
+	ActionName(sub.Path("/users/{user_id}").Methods("PUT", "PATCH").HandlerFunc(somebody), "Web::Users#update")
+	ActionName(sub.Path("/users/{user_id}").Methods("DELETE").HandlerFunc(somebody), "Web::Users#destroy")
+	ActionName(sub.Path("/users/{user_id}/comrades").Methods("GET").HandlerFunc(somebody), "Web::Users#comrades")
+
+	router.Path("/allmethods").HandlerFunc(somebody)
+	router.Path("/simple").Methods("GET").HandlerFunc(somebody)
 
 	fs, _ := os.Open(os.DevNull)
 	logger := log.New(fs, "API", log.LstdFlags|log.Lshortfile)
@@ -47,26 +56,21 @@ func TestGorillaNameExtraction(t *testing.T) {
 	server := httptest.NewServer(logjam.NewMiddleware(router))
 	defer server.Close()
 
-	Convey("setting up action name extraction using gorilla", t, func() {
-		fmt.Println()
-		SetupActionNameExtraction(router, &Options{CheckOnly: true})
-	})
+	socket, err := zmq4.NewSocket(zmq4.ROUTER)
+	if err != nil {
+		panic("cannot bind socket for testing")
+	}
+	socket.Bind("tcp://*:9604")
+	defer func() {
+		socket.Unbind("tcp://*:9604")
+	}()
 
-	Convey("extracting action names using gorilla", t, func() {
-		SetupActionNameExtraction(router, &Options{})
-
-		socket, err := zmq4.NewSocket(zmq4.ROUTER)
-		So(err, ShouldBeNil)
-		socket.Bind("tcp://*:9604")
-		defer func() {
-			socket.Unbind("tcp://*:9604")
-		}()
-
-		req, err := http.NewRequest("GET", server.URL+"/rest/users/123", nil)
+	performAndCheck := func(method string, path string, expectedResonseCode int, expectedActionName string) {
+		req, err := http.NewRequest(method, server.URL+path, nil)
 		res, err := server.Client().Do(req)
 
 		So(err, ShouldBeNil)
-		So(res.StatusCode, ShouldEqual, 200)
+		So(res.StatusCode, ShouldEqual, expectedResonseCode)
 
 		msg, err := socket.RecvMessage(0)
 		So(err, ShouldBeNil)
@@ -78,25 +82,27 @@ func TestGorillaNameExtraction(t *testing.T) {
 		output := map[string]interface{}{}
 		json.Unmarshal([]byte(payload), &output)
 
-		So(output["action"], ShouldEqual, "Rest::Users#get")
+		So(output["action"], ShouldEqual, expectedActionName)
+	}
 
-		req, err = http.NewRequest("GET", server.URL+"/rest/schnurzes/123/users", nil)
-		res, err = server.Client().Do(req)
+	Convey("setting up action name extraction using gorilla", t, func() {
 
-		So(err, ShouldBeNil)
-		So(res.StatusCode, ShouldEqual, 200)
+		SetupRoutes(router, &Options{CheckOnly: true})
+		PrintRoutes(router)
 
-		msg, err = socket.RecvMessage(0)
-		So(err, ShouldBeNil)
-		So(msg, ShouldHaveLength, 5)
+	})
 
-		payload, err = snappy.Decode(nil, []byte(msg[3]))
-		So(err, ShouldBeNil)
+	Convey("defining action names using gorilla", t, func() {
 
-		output = map[string]interface{}{}
-		json.Unmarshal([]byte(payload), &output)
+		SetupRoutes(router, &Options{})
 
-		So(output["action"], ShouldEqual, "Rest::Schnurzes#users")
+		performAndCheck("GET", "/rest/users/123", 200, "Rest::Users#show")
+		performAndCheck("DELETE", "/rest/users/123", 200, "Rest::Users#destroy")
+		performAndCheck("PUT", "/rest/users/123", 200, "Rest::Users#update")
+		performAndCheck("GET", "/rest/users/123/comrades", 200, "Rest::Users#comrades")
+		performAndCheck("GET", "/web", 404, "Unknown#web")
+		performAndCheck("GET", "/simple", 200, "Simple#get")
+		performAndCheck("POST", "/allmethods", 200, "Allmethods#post")
 
 	})
 }
